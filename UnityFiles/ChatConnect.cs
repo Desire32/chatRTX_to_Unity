e33a16acb3fbc632975e92837db03ced1086c8be
nmarkov <58604764+Desire32@users.noreply.github.com>
2024-08-05 23:44:39 +0300

    Update ChatConnect.cs
    Adding another IEnumerator for more sufficient work (might be not needed, considered as a backup for now)

--- UnityFiles/ChatConnect.cs
+++ UnityFiles/ChatConnect.cs
@@ -1,4 +1,3 @@
-﻿// libs
 using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
@@ -20,6 +19,7 @@ public class ChatConnect : MonoBehaviour
     public UserInput inputfield;
     public TextMeshProUGUI botResponseText;
     public TextMeshProUGUI botSortedText;
+    string url = "http://127.0.0.1:5000/send_message";
 
     void Start()
     {
@@ -37,7 +37,7 @@ public class ChatConnect : MonoBehaviour
     {
         if (inputfield != null && !string.IsNullOrEmpty(inputfield.userInput))
         {
-            StartCoroutine(SendMessageCoroutine(inputfield.userInput));
+            StartCoroutine(SendMessageCoroutine(inputfield.userInput, url));
         }
         else
         {
@@ -45,72 +45,71 @@ public class ChatConnect : MonoBehaviour
         }
     }
 
-IEnumerator SendMessageCoroutine(string message)
-{
-    // Send the initial message to the server       
-    string url = "http://127.0.0.1:5000/send_message";
-    string messageJson = $"{{\"message\": \"{message}\"}}";
-    UnityWebRequest request = new UnityWebRequest(url, "POST");
-    byte[] bodyRaw = Encoding.UTF8.GetBytes(messageJson);
-    request.uploadHandler = new UploadHandlerRaw(bodyRaw);
-    request.downloadHandler = new DownloadHandlerBuffer();
-    request.SetRequestHeader("Content-Type", "application/json");
-
-    yield return request.SendWebRequest();
-
-    if (request.result != UnityWebRequest.Result.Success)
+    IEnumerator SendMessageCoroutine(string message, string url)
     {
-        Debug.Log(request.error);
-    }
-    else
-    {
-        // Handle the response from the server
-        Debug.Log("Response: " + request.downloadHandler.text);
-        string responseJson = request.downloadHandler.text;
-        BotResponse responseObject = JsonUtility.FromJson<BotResponse>(responseJson);
+        // Send the initial message to the server
+        string messageJson = $"{{\"message\": \"{message}\"}}";
+        UnityWebRequest request = new UnityWebRequest(url, "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(messageJson);
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
 
-        // If the response is valid, prepare a new message
-        if (responseObject != null)
+        yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
         {
-            string preparedMessage = $"Divide this text into three smaller topics: {responseObject.response}";
-            string preloadedJSON = $"{{\"message\": \"{preparedMessage}\"}}";
-            UnityWebRequest preparedRequest = new UnityWebRequest(url, "POST");
-            byte[] bodyFresh = Encoding.UTF8.GetBytes(preloadedJSON);
-            preparedRequest.uploadHandler = new UploadHandlerRaw(bodyFresh);
-            preparedRequest.downloadHandler = new DownloadHandlerBuffer();
-            preparedRequest.SetRequestHeader("Content-Type", "application/json");
+            Debug.Log(request.error);
+        }
+        else
+        {
+            // Handle the response from the server
+            Debug.Log("Response: " + request.downloadHandler.text);
+            string responseJson = request.downloadHandler.text;
+            BotResponse responseObject = JsonUtility.FromJson<BotResponse>(responseJson);
 
-            yield return preparedRequest.SendWebRequest();
-
-            if (preparedRequest.result != UnityWebRequest.Result.Success)
+            if (botResponseText != null)
             {
-                Debug.Log(preparedRequest.error);
+                botResponseText.text = responseObject.response;
             }
-            else
+
+            // If the response is valid, prepare a new message
+            if (responseObject != null)
             {
-                Debug.Log("Sorted Response: " + preparedRequest.downloadHandler.text);
-                BotResponse sortedResponseObject = JsonUtility.FromJson<BotResponse>(preparedRequest.downloadHandler.text);
+                string preparedMessage = $"Divide this text into three smaller topics: 1. Name of the topic, 2. Main information, " +
+                        $"3. Interesting fact (if there is one) {responseObject.response}";
+                StartCoroutine(SendPreparedMessage(preparedMessage, url));
+            }
+            // Clear the input field
+            inputfield.ClearInputField();
+        }
+    }
+    IEnumerator SendPreparedMessage(string preparedMessage, string url)
+    {
+        string preloadedJSON = $"{{\"message\": \"{preparedMessage}\"}}";
+        UnityWebRequest preparedRequest = new UnityWebRequest(url, "POST");
+        byte[] bodyFresh = Encoding.UTF8.GetBytes(preloadedJSON);
+        preparedRequest.uploadHandler = new UploadHandlerRaw(bodyFresh);
+        preparedRequest.downloadHandler = new DownloadHandlerBuffer();
+        preparedRequest.SetRequestHeader("Content-Type", "application/json");
 
-                // Update the UI with the sorted response
-                if (botResponseText != null)
-                {
-                        botResponseText.text = responseObject.response;
-                }
-                // Second UI Unity object
-                if(botSortedText != null)
-                    {
-                        botSortedText.text = sortedResponseObject.response;
-                    }
+        yield return preparedRequest.SendWebRequest();
 
-                else
-                {
-                    Debug.LogError("BotResponseText is not assigned.");
-                }
+        if (preparedRequest.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError(preparedRequest.error);
+        }
+        else
+        {
+            Debug.Log("Sorted Response: " + preparedRequest.downloadHandler.text);
+            BotResponse sortedResponseObject = JsonUtility.FromJson<BotResponse>(preparedRequest.downloadHandler.text);
+
+            // Update the UI with the sorted response
+            if (botSortedText != null)
+            {
+                botSortedText.text = sortedResponseObject.response;
             }
         }
 
-        // Clear the input field
-        inputfield.ClearInputField();
     }
-}
 }
